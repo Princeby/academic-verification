c18969a42dbd87e73d4455a7b0f10c5b035635b7
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2025-12-21 00:35:29 +0100

    Added institutions calls to my pallet

--- pallets/did/src/lib.rs
+++ pallets/did/src/lib.rs
@@ -95,7 +95,7 @@ pub mod pallet {
     pub struct Institution<T: Config> {
         pub name: BoundedVec<u8, T::MaxDocumentSize>,
         pub did: T::AccountId,
-        pub verfified: bool,
+        pub verified: bool,
         pub registered_at: BlockNumberFor<T>,
     }
 
@@ -184,7 +184,210 @@ pub mod pallet {
 
         }
 
+        //Add a new public key
+        /// Add a new public key to an existing DID
+        #[pallet::call_index(1)]
+        #[pallet::weight(T::WeightInfo::add_public_key())]
+        pub fn add_public_key(
+            origin: OriginFor<T>,
+            public_key: [u8; 32],
+            key_type: KeyType,
+        ) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            DidDocuments::<T>::try_mutate(&who, |did_doc_opt| -> DispatchResult {
+                let did_doc = did_doc_opt.as_mut()
+                    .ok_or(Error::<T>::DidNotFound)?;
+
+                ensure!(did_doc.controller == who, Error::<T>::NotAuthorized);
+                ensure!(did_doc.active, Error::<T>::DidInactive);
+
+                let key_id = hashing::blake2_256(&public_key);
+                ensure!(
+                    !did_doc.public_keys.iter().any(|k| k.key_id == key_id),
+                    Error::<T>::PublicKeyAlreadyExists
+                );
+
+                let key_entry = PublicKeyEntry {
+                    key_id,
+                    key_type,
+                    public_key,
+                };
+
+                did_doc.public_keys.try_push(key_entry)
+                    .map_err(|_| Error::<T>::TooManyPublicKeys)?;
+
+                did_doc.updated_at = frame_system::Pallet::<T>::block_number();
+
+                Self::deposit_event(Event::PublicKeyAdded { 
+                    owner: who.clone(), 
+                    key_id 
+                });
+
+                Ok(())
+            })
+        }
+
+        //Remove a public key from a DID
+        #[pallet::call_index(2)]
+        #[pallet::weight(T::WeightInfo::remove_public_key())]
+        pub fn remove_public_key(
+            origin: OriginFor<T>,
+            key_id: [u8; 32],
+        ) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            DidDocuments::<T>::try_mutate(&who, |did_doc_opt| -> DispatchResult {
+                let did_doc = did_doc_opt.as_mut()
+                    .ok_or(Error::<T>::DidNotFound)?;
+
+                ensure!(did_doc.controller == who, Error::<T>::NotAuthorized);
+
+                let initial_len = did_doc.public_keys.len();
+                did_doc.public_keys.retain(|k| k.key_id != key_id);
+
+                ensure!(
+                    did_doc.public_keys.len() < initial_len,
+                    Error::<T>::PublicKeyNotFound
+                );
+
+                did_doc.updated_at = frame_system::Pallet::<T>::block_number();
+
+                Self::deposit_event(Event::PublicKeyRemoved { 
+                    owner: who.clone(), 
+                    key_id 
+                });
+
+                Ok(())
+            })
+        }
+
+        /// Deactivate a DID
+        #[pallet::call_index(3)]
+        #[pallet::weight(T::WeightInfo::deactivate_did())]
+        pub fn deactivate_did(origin: OriginFor<T>) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            DidDocuments::<T>::try_mutate(&who, |did_doc_opt| -> DispatchResult {
+                let did_doc = did_doc_opt.as_mut()
+                    .ok_or(Error::<T>::DidNotFound)?;
+
+                ensure!(did_doc.controller == who, Error::<T>::NotAuthorized);
+
+                did_doc.active = false;
+                did_doc.updated_at = frame_system::Pallet::<T>::block_number();
+
+                Self::deposit_event(Event::DidDeactivated { owner: who.clone() });
+
+                Ok(())
+            })
+        }
+
+        /// Reactivate a deactivated DID
+        #[pallet::call_index(4)]
+        #[pallet::weight(T::WeightInfo::reactivate_did())]
+        pub fn reactivate_did(origin: OriginFor<T>) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            DidDocuments::<T>::try_mutate(&who, |did_doc_opt| -> DispatchResult {
+                let did_doc = did_doc_opt.as_mut()
+                    .ok_or(Error::<T>::DidNotFound)?;
+
+                ensure!(did_doc.controller == who, Error::<T>::NotAuthorized);
+
+                did_doc.active = true;
+                did_doc.updated_at = frame_system::Pallet::<T>::block_number();
+
+                Self::deposit_event(Event::DidUpdated { owner: who.clone() });
+
+                Ok(())
+            })
+        }
+
+        /// Register as an academic institution
+        #[pallet::call_index(5)]
+        #[pallet::weight(T::WeightInfo::register_institution())]
+        pub fn register_institution(
+            origin: OriginFor<T>,
+            name: BoundedVec<u8, T::MaxDocumentSize>,
+        ) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            ensure!(
+                !Institutions::<T>::contains_key(&who), 
+                Error::<T>::InstitutionAlreadyRegistered
+            );
+
+            ensure!(
+                DidDocuments::<T>::contains_key(&who), 
+                Error::<T>::DidNotFound
+            );
+
+            ensure!(!name.is_empty(), Error::<T>::InvalidInstitutionName);
+
+            let institution = Institution {
+                name: name.clone(),
+                did: who.clone(),
+                verified: false,
+                registered_at: frame_system::Pallet::<T>::block_number(),
+            };
+
+            Institutions::<T>::insert(&who, institution);
+
+            Self::deposit_event(Event::InstitutionRegistered { 
+                did: who, 
+                name 
+            });
+
+            Ok(())
+        }
+
+        /// Verify an institution (governance/root only)
+        #[pallet::call_index(6)]
+        #[pallet::weight(T::WeightInfo::verify_institution())]
+        pub fn verify_institution(
+            origin: OriginFor<T>,
+            institution_did: T::AccountId,
+        ) -> DispatchResult {
+            ensure_root(origin)?;
+
+            Institutions::<T>::try_mutate(&institution_did, |institution_opt| -> DispatchResult {
+                let institution = institution_opt.as_mut()
+                    .ok_or(Error::<T>::InstitutionNotFound)?;
+                
+                institution.verified = true;
+
+                Self::deposit_event(Event::InstitutionVerified { 
+                    did: institution_did.clone() 
+                });
+
+                Ok(())
+            })
+        }
+
+        /// Revoke institution verification (governance/root only)
+        #[pallet::call_index(7)]
+        #[pallet::weight(T::WeightInfo::revoke_institution())]
+        pub fn revoke_institution(
+            origin: OriginFor<T>,
+            institution_did: T::AccountId,
+        ) -> DispatchResult {
+            ensure_root(origin)?;
+
+            Institutions::<T>::try_mutate(&institution_did, |institution_opt| -> DispatchResult {
+                let institution = institution_opt.as_mut()
+                    .ok_or(Error::<T>::InstitutionNotFound)?;
+                
+                institution.verified = false;
+
+                Self::deposit_event(Event::InstitutionRevoked { 
+                    did: institution_did.clone() 
+                });
+
+                Ok(())
+            })
+        }
+
+        
     }
-
-
 }
